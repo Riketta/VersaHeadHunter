0e4535316dee8f7be6f19e2fe642fffa03ad1a6d
Evgeny 'Riketta' Chugaev <riketta@outlook.com>
2022-06-09 13:02:19 +0300

    Token removed

--- VersaHeadHunter/Program.cs
+++ VersaHeadHunter/Program.cs
@@ -18,7 +18,7 @@ namespace VersaHeadHunter
             "https://www.wowprogress.com/gearscore/eu/%D1%81%D0%B2%D0%B5%D0%B6%D0%B5%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C-%D0%B4%D1%83%D1%88?lfg=1&sortby=ts", // СД
             "https://www.wowprogress.com/gearscore/eu/%D0%B3%D0%BE%D1%80%D0%B4%D1%83%D0%BD%D0%BD%D0%B8?lfg=1&sortby=ts", // Гордунни
         };
-        private readonly static string token = "ODExNjg0MjAzNTg5NjY0ODE4.YC1x0g.dRwLtzqC6kAu7lva0_mzjXOs1LU";
+        private readonly static string token = "___";
         private readonly static ulong channelId = 649913439556206592; // channel used to post messages into
         
         private readonly DiscordSocketClient _client;
