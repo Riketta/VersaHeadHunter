2937c49bfa61594a2656f1e4e4b0f03ca22a613e
Evgeny 'Riketta' Chugaev <riketta@outlook.com>
2022-06-09 13:11:31 +0300

    Reading token from local file

--- VersaHeadHunter/Program.cs
+++ VersaHeadHunter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Discord;
@@ -18,7 +19,7 @@ namespace VersaHeadHunter
             "https://www.wowprogress.com/gearscore/eu/%D1%81%D0%B2%D0%B5%D0%B6%D0%B5%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C-%D0%B4%D1%83%D1%88?lfg=1&sortby=ts", // СД
             "https://www.wowprogress.com/gearscore/eu/%D0%B3%D0%BE%D1%80%D0%B4%D1%83%D0%BD%D0%BD%D0%B8?lfg=1&sortby=ts", // Гордунни
         };
-        private readonly static string token = "___";
+        private readonly static string token = File.ReadAllText("token.txt");
         private readonly static ulong channelId = 649913439556206592; // channel used to post messages into
         
         private readonly DiscordSocketClient _client;
